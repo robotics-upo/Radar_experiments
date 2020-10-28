5da2fd2f3afdf0222c843ab3be616c35e623e7f0
david-alejo <churr.te@gmail.com>
2020-10-28 18:39:54 +0100

    Added scripts to automatize tests

---
+++ scripts/localize_lidar_bag_fov30.bash
@@ -0,0 +1,18 @@
+
+
+
+if [ $# -lt 2 ]; then
+  echo "Usage: $0 <first_exe_number> <last_exe_number> [<output_stats_folder>]"
+  exit 1
+fi
+
+CONTADOR=$1
+
+if [ $# -gt 2 ]; then
+  echo "Saving stats to: $3"
+fi
+
+until [ $CONTADOR -gt $2 ]; do
+  roslaunch localize_lidar.launch initial_x:=5.9 initial_y:=10.46 initial_a:=-1.59 bag_name:=multi_radarfov30_corrected.bag stats_file:=$3stats_lidar_fov30_$CONTADOR.txt
+  let CONTADOR+=1
+done
---
+++ scripts/localize_radar_bag_fov40.bash
@@ -0,0 +1,1 @@
+roslaunch localize_radar.launch initial_x:=6.2 initial_y:=11.1 initial_a:=-1.59 bag_name:=multi_radar_fov40_corrected.bag
