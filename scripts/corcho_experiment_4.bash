83d672115b3236440cdc4585d9402549213f9910
david-alejo <churr.te@gmail.com>
2020-11-20 15:45:37 +0100

    First version with radar OK in corchopan

---
+++ scripts/corcho_experiment_4.bash
@@ -0,0 +1,26 @@
+
+
+
+if [ $# -lt 2 ]; then
+  echo "Usage: $0 <first_exe_number> <last_exe_number> [<output_stats_folder>]"
+  exit 1
+fi
+
+CONTADOR=$1
+
+if [ $# -gt 2 ]; then
+  echo "Saving stats to: $3"
+fi
+
+X=6.2
+Y=11.55
+A=-1.5
+BAG=$HOME/Humo/corchopan/2020-11-11-14-50-00_corcho_humodenso2.bag
+
+# roslaunch radar_experiments localize_gt.launch bag_file:=$BAG stats_file:=stats_corcho_gt_4.txt map_name:=map_corchopan_v1.txt
+
+until [ $CONTADOR -gt $2 ]; do
+  roslaunch radar_experiments localize_lidar.launch initial_x:=$X initial_y:=$Y initial_a:=$A bag_file:=$BAG stats_file:=stats_corcho_lidar_4_$CONTADOR.txt map_name:=corchopan_v2.bt
+  roslaunch radar_experiments localize_radar.launch initial_x:=$X initial_y:=$Y initial_a:=$A bag_file:=$BAG stats_file:=stats_corcho_radar_4_$CONTADOR.txt map_name:=corchopan_v2.bt
+  let CONTADOR+=1
+done
