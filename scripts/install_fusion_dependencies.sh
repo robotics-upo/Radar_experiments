1acce582da707c35c775e83507d6e88f50edd0b8
david-alejo <churr.te@gmail.com>
2021-03-05 14:26:05 +0100

    Added fiducals dependency and g2o installation option

--- scripts/install_fusion_dependencies.sh
+++ scripts/install_fusion_dependencies.sh
@@ -13,13 +13,21 @@ cd "$catkin_path"/src || (echo "ERROR: Couldn't go to the catkin path: $catkin_p
 # Cloud concatenator, odom to tf, nix driver
 
 echo -e "${YELLOW}    ---- Clonning NIx Driver ...${NC}"
-git clone https://github.com/robotics-upo/NIx_driver -b radar_experiemnts || ( echo "Stashing your changings and checking out to radar experiments branch... "  && cd NIx_driver && git stash && git checkout radar_experiemnts && git pull && cd ..)
+git clone https://github.com/robotics-upo/NIx_driver -b radar_experiments || ( echo "Stashing your changings and checking out to radar experiments branch... "  && cd NIx_driver && git stash && git checkout radar_experiemnts && git pull && cd ..)
 echo -e "${YELLOW}    ---- Clonning Odom to tf  ...${NC}"
 git clone https://github.com/robotics-upo/odom_to_tf -b develop           || ( echo "Stashing your changings and checking out to develop branch... " && cd odom_to_tf && git stash && git checkout develop && git pull && cd ..)
 echo -e "${YELLOW}    ---- Clonning Cloud Concatenator  ...${NC}"
 git clone https://github.com/robotics-upo/cloud_concatenator              || ( echo "Stashing your changes and pulling latest commit... " && cd cloud_concatenator && git stash && git pull && cd .. )
 echo -e "${YELLOW}    ---- Clonning UPO Markers  ...${NC}"
 git clone https://github.com/robotics-upo/upo_markers                     || ( echo "Stashing your changes and pulling latest commit... "  && cd upo_markers && git stash && git pull && cd .. )
+echo -e "${YELLOW}    ---- Clonning Fiducials  ...${NC}"
+git clone https://github.com/robotics-upo/fiducials                     || ( echo "Stashing your changes and pulling latest commit... "  && cd upo_markers && git stash && git pull && cd .. )
+
+read -p "-- Installing dependencies of fiducials: Installing G2O. Proceed (y/N) (enter not if you have already installed G2O in your system)" -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+    "$catkin_path"/src/fiducials/fiducial_slam/scripts/g2o_installation.sh
+fi
 
 echo -e "${YELLOW}-- Repositories cloned, compiling...${NC}"
 
@@ -29,9 +37,10 @@ catkin_make
 echo -e "${YELLOW}-- Next step is to download bags from dropbox. You need at least 20 GB of disk space ...${NC}"
 path="radar_fusion_bags"
 
-read -p "Do you want to continue? (Y/n)" -n 1 -r
-echo  
-if [[ $REPLY =~ ^[Yy]$ ]]; then
+read -p "Do you want to continue? (y/N)" -n 1 -r
+echo 
+echo $REPLY
+if [[ $REPLY =~ [Yy] ]]; then
     echo -e "${YELLOW}-- Starting bags download${NC}"
     mkdir ~/"$path"
     cd ~/"$path" || (echo "ERROR: Couldn't go to downlaod location: ~/$path " && exit )
