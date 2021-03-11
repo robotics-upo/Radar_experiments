d284d62aeaf75d3da1ec70c438892b5e8d907316
david-alejo <churr.te@gmail.com>
2021-03-11 18:29:01 +0100

    Added experiments with raw radar,lidar

---
+++ scripts/experiment_batch.bash
@@ -0,0 +1,10 @@
+#! /bin/bash
+CONTADOR=$1
+until [ $CONTADOR -gt $2 ]; do
+  # Roslaunch with multiple parameters
+  echo Launch experiment $CONTADOR
+  # roslaunch radar_experiments localize_with_gt.launch gt_filename:=stats_fusion_$CONTADOR.txt
+  roslaunch radar_experiments localize_with_gt.launch gt_filename:=stats_lidar_$CONTADOR.txt localize_method:=radar
+  roslaunch radar_experiments localize_with_gt.launch gt_filename:=stats_radar_$CONTADOR.txt localize_method:=lidar
+  let CONTADOR+=1
+done
