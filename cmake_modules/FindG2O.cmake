7635bd7f49f877825019dee11c07707f66228015
david-alejo <churr.te@gmail.com>
2021-05-12 19:25:12 +0200

    Fixed FindG2o.cmake

--- cmake_modules/FindG2O.cmake
+++ cmake_modules/FindG2O.cmake
@@ -68,10 +68,10 @@ IF(UNIX)
                     ${G2O_STUFF_LIB}          
                     ${G2O_TYPES_SLAM2D_LIB}   
                     ${G2O_TYPES_SLAM3D_LIB}   
-                    ${G2O_SOLVER_CHOLMOD_LIB} 
+#                    ${G2O_SOLVER_CHOLMOD_LIB} 
                     ${G2O_SOLVER_PCG_LIB}     
-                    ${G2O_SOLVER_CSPARSE_LIB} 
-                    ${G2O_INCREMENTAL_LIB}                        
+#                    ${G2O_SOLVER_CSPARSE_LIB} 
+#                    ${G2O_INCREMENTAL_LIB}                        
                     )
 
   IF(G2O_LIBRARIES AND G2O_INCLUDE_DIR)
