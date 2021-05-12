8deaadc6d7eab972bb23d12393baf75a021a2f81
david-alejo <churr.te@gmail.com>
2021-05-12 16:24:16 +0200

    Added dll

--- scripts/experiment_batch.bash
+++ scripts/experiment_batch.bash
@@ -4,7 +4,7 @@ until [ $CONTADOR -gt $2 ]; do
   # Roslaunch with multiple parameters
   echo Launch experiment $CONTADOR
   roslaunch radar_experiments localize_with_gt.launch gt_filename:=stats_fusion_$CONTADOR.txt localize_method:=fusion
-  roslaunch radar_experiments localize_with_gt.launch gt_filename:=stats_radar_$CONTADOR.txt localize_method:=radar
-  roslaunch radar_experiments localize_with_gt.launch gt_filename:=stats_lidar_$CONTADOR.txt localize_method:=lidar
+  # roslaunch radar_experiments localize_with_gt.launch gt_filename:=stats_radar_dll_$CONTADOR.txt localize_method:=radar
+  # roslaunch radar_experiments localize_with_gt.launch gt_filename:=stats_lidar_$CONTADOR.txt localize_method:=lidar
   let CONTADOR+=1
 done
