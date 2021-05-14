a1522e5aa85275098c7e035febc6e01e4518e990
david-alejo <churr.te@gmail.com>
2021-05-14 10:54:08 +0200

    Version for the bag 12-01-11-36

--- scripts/experiment_batch_11.bash
+++ scripts/experiment_batch_11.bash
@@ -3,12 +3,13 @@ CONTADOR=$1
 until [ $CONTADOR -gt $2 ]; do
   # Roslaunch with multiple parameters
   echo Launch experiment $CONTADOR
-  export X=6.3
-  export Y=4.6
-  export A=1.6
+  export X=6.1
+  export Y=4.4
+  export A=1.4
 
-  roslaunch radar_experiments localize_with_gt.launch gt_filename:=stats_fusion_11_$CONTADOR.txt localize_method:=fusion bag_name:=2020-12-01-11-36-21_odom_gt.bag initial_pose_x=$X initial_pose_y=$X initial_pose_a=$A
-  roslaunch radar_experiments localize_with_gt.launch gt_filename:=stats_radar_11_$CONTADOR.txt localize_method:=radar scan_topic:=radar_scan bag_name:=2020-12-01-11-36-21_odom_gt.bag initial_pose_x=$X initial_pose_y=$X initial_pose_a=$A
-  roslaunch radar_experiments localize_with_gt.launch gt_filename:=stats_lidar_11_$CONTADOR.txt localize_method:=lidar scan_topic:=lidar_scan bag_name:=2020-12-01-11-36-21_odom_gt.bag initial_pose_x=$X initial_pose_y=$X initial_pose_a=$A
+  roslaunch radar_experiments localize_with_gt.launch gt_filename:=stats_fusion_11_$CONTADOR.txt localize_method:=fusion bag_name:=2020-12-01-11-36-21_odom_gt.bag initial_pose_x:=$X initial_pose_y:=$Y initial_pose_a:=$A
+  roslaunch radar_experiments localize_with_gt.launch gt_filename:=stats_lidar_11_$CONTADOR.txt localize_method:=lidar bag_name:=2020-12-01-11-36-21_odom_gt.bag initial_pose_x:=$X initial_pose_y:=$Y initial_pose_a:=$A scan_topic:=/lidar_scan
+  roslaunch radar_experiments localize_with_gt.launch gt_filename:=stats_radar_11_$CONTADOR.txt localize_method:=radar bag_name:=2020-12-01-11-36-21_odom_gt.bag initial_pose_x:=$X initial_pose_y:=$Y initial_pose_a:=$A scan_topic:=/radar_scan
+
   let CONTADOR+=1
 done
