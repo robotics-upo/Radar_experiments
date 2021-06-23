d536d2352f7033c9e252f7e6f9aa113c251d7a15
david-alejo <churr.te@gmail.com>
2021-06-23 09:49:11 +0200

    Improved plot generation

--- octave/get_stats.m
+++ octave/get_stats.m
@@ -1,36 +1,38 @@
 function stats=get_stats(data, dt)
-  max_time = 1e11
-  time_scale = 1e-9
+  max_time = 1e11;
+  time_scale = 1e-9;
  
   for i=1:length(data)
     max_time_ = floor(data{i}(length(data{i}(:,1)),1)*time_scale);
-    max_time = min([max_time, max_time_])
-    i
+    max_time = min([max_time, max_time_]);
+    i;
   end
   
   printf('Max time: %d\n', max_time)
   
-  stats=zeros(max_time + 1, 6); # primera y segunda fila mean std_dev error m
+  stats=zeros(1, 6); # primera y segunda fila mean std_dev error m
                                # tercera y cuarta, mean std_dev error rad
+                               # quinta y sexta: incertidumbre AMCL
   
   indices = ones(length(data), 1);
   
-  for i=0:max_time:dt
+  for i=0:dt:max_time
     
     eff = 0;
     curr_data_vec = zeros(1,4);
+    k = floor(i/dt);
     for j=1:length(data)
       curr_data = zeros(4,1);  
-      curr_t = floor(data{j}(indices(j))*time_scale/dt);
+      curr_t = floor(data{j}(indices(j))*time_scale);
       %printf('Time: %d Curr_t: %d\n', i, curr_t)
       n = 0;
-      while (curr_t == i) 
+      
+      while (floor(curr_t/dt) == k) 
         n++;
         curr_data(1,n) = data{j}(indices(j),8); # 8 es el error distancia
         curr_data(2,n) = data{j}(indices(j),9); # 9 es el error yaw
-        curr_data(3,n) = sqrt( data{j}(indices(j),10)*data{j}(indices(j),10) + data{j}(indices(j),11)*data{j}(indices(j),11));
-        curr_data(4,n) = data{j}(indices(j),12);
-        
+        curr_data(3,n) = sqrt( data{j}(indices(j),10) + data{j}(indices(j),11));
+        curr_data(4,n) = sqrt(data{j}(indices(j),12));
         indices(j)++;
         curr_t = floor(data{j}(indices(j))*time_scale);
       endwhile
@@ -42,12 +44,13 @@ function stats=get_stats(data, dt)
         curr_data_vec(eff, 4 )=mean(curr_data(2,:));  
       end  
     end
-    stats(i+1,1) = mean(curr_data_vec(:,1));
-    stats(i+1,2) = std(curr_data_vec(:,1));
-    stats(i+1,3) = mean(curr_data_vec(:,2));
-    stats(i+1,4) = std(curr_data_vec(:,2));
-    stats(i+1,5) = mean(curr_data_vec(:,3));
-    stats(i+1,6) = mean(curr_data_vec(:,4));
+    stats(k+1,1) = mean(curr_data_vec(:,1));
+    stats(k+1,2) = std(curr_data_vec(:,1));
+    stats(k+1,3) = mean(curr_data_vec(:,2));
+    stats(k+1,4) = std(curr_data_vec(:,2));
+    stats(k+1,5) = mean(curr_data_vec(:,3));
+    stats(k+1,6) = mean(curr_data_vec(:,4));
+    stats(k+1,7) = i;
   end
   
 end
--- octave/process_data.m
+++ octave/process_data.m
@@ -1,41 +1,79 @@
 close all % Close all figures and start it over!
-figure(1);
+
+% Parameters
+fontsize = 18
+experiment='_11_'
+lw = 2
 dt = 20
+
+left = 0.15;
+width = 0.7;
+
+plots = 4;
+
+margin = 0.1
+mini_margin = 0.02
+v_size = (1 - 2*margin - plots * mini_margin) / 4
+
+pos = 3*(v_size + mini_margin) + margin
+hax1 = subplot ("position", [left pos width v_size]);
+
 addpath('/home/chur/fiducial_ws/src/Radar_experiments/octave');
 cd ../stats
-data_dbscan = load_data(1,10,'stats_dbscan_fusion_11_');
-a = get_stats(data_dbscan);
-plot(a(:,1));-+
-data_lidar = load_data(1,10,'stats_lidar_11_');
-b = get_stats(data_lidar);
+data_dbscan = load_data(1,10,strcat('stats_dbscan_fusion',experiment));
+a = get_stats(data_dbscan, dt);
+plot(a(:,7), a(:,1), "linewidth", lw);
+data_lidar = load_data(1,10,strcat('stats_lidar',experiment));
+b = get_stats(data_lidar, dt);
 hold on;
-plot(b(:,1),'g');
-data_fusion = load_data(1,10,'stats_fusion_11_');
-c = get_stats(data_fusion);
-plot(c(:,1),'r');
-data_radar = load_data(1,10,'stats_radar_11_');
-d = get_stats(data_radar);
-plot(d(:,1),'k');
+plot(b(:,7), b(:,1),'g', "linewidth", lw);
+data_fusion = load_data(1,10,strcat('stats_fusion',experiment));
+c = get_stats(data_fusion, dt);
+plot(c(:,7), c(:,1),'r', "linewidth", lw);
+data_radar = load_data(1,10,strcat('stats_radar',experiment));
+d = get_stats(data_radar, dt);
+plot(d(:,7), d(:,1),'k', "linewidth", lw);
+axis("labely")
+hy1 = ylabel("Position error (m)")
+set (hy1, "fontsize", fontsize) 
 
-figure(2)
-plot(a(:,2),'b');
+%figure(2)
+pos = 2*(v_size + mini_margin) + margin
+hax2 = subplot ("position", [left pos width v_size]);
+plot(a(:,7), a(:,2),'b', "linewidth", lw);
 hold on;
-plot(b(:,2),'g');
-plot(c(:,2),'r');
-plot(d(:,2),'k');
-
+plot(b(:,7), b(:,2),'g', "linewidth", lw);
+plot(c(:,7), c(:,2),'r', "linewidth", lw);
+plot(d(:,7), d(:,2),'k', "linewidth", lw);
+axis("labely")
+hy2 = ylabel("Orientation error (rad)")
+set (hy2, "fontsize", fontsize) 
 
 % Plot uncertainty AMCL dist
-figure(3)
-plot(a(:,3),'b');
+pos = 1*(v_size + mini_margin) + margin
+hax3 = subplot ("position", [left pos width v_size]);
+plot(a(:,7), a(:,3),'b', "linewidth", lw);
 hold on;
-plot(b(:,3),'g');
-plot(c(:,3),'r');
-plot(d(:,3),'k');
+plot(b(:,7), b(:,3),'g', "linewidth", lw);
+plot(c(:,7), c(:,3),'r', "linewidth", lw);
+plot(d(:,7), d(:,3),'k', "linewidth", lw);
+axis("labely")
+hy3 = ylabel("AMCL uncert. (m)")
+set (hy3, "fontsize", fontsize) 
 
-figure(4)
-plot(a(:,4),'b');
+hax4 = subplot ("position", [left margin width v_size]);
+plot(a(:,7), a(:,4),'b', "linewidth", lw);
 hold on;
-plot(b(:,4),'g');
-plot(c(:,4),'r');
-plot(d(:,4),'k');
+plot(b(:,7), b(:,4),'g', "linewidth", lw);
+plot(c(:,7), c(:,4),'r', "linewidth", lw);
+plot(d(:,7), d(:,4),'k', "linewidth", lw);
+hx1 = xlabel ("Time (s)");
+set (hx1, "fontsize", fontsize) 
+hy4 = ylabel("AMCL uncert. (rad)")
+linkaxes ([hax1, hax2, hax3, hax4],"x");
+set (hy4, "fontsize", fontsize) 
+set(hax4, "fontsize", fontsize, "linewidth", 2);
+set(hax3, "fontsize", fontsize, "linewidth", 2);
+set(hax2, "fontsize", fontsize, "linewidth", 2);
+set(hax1, "fontsize", fontsize, "linewidth", 2);
+
