b325a8743a649f29f9fefc5b083a6d242d07fe52
david-alejo <churr.te@gmail.com>
2021-07-13 16:17:45 +0200

    Added number of repetitions as a parameter

--- octave/process_data.m
+++ octave/process_data.m
@@ -2,7 +2,10 @@ close all % Close all figures and start it over!
 
 % Parameters
 fontsize = 18
-experiment='_11_'
+#experiment='_11_'   # or ...
+#experiment = '_27_nov_12_57_'
+experiment = '_27_nov_12_57_'
+n_max = 5
 lw = 2
 dt = 20
 
@@ -20,17 +23,24 @@ hax1 = subplot ("position", [left pos width v_size]);
 
 addpath('/home/chur/fiducial_ws/src/Radar_experiments/octave');
 cd ../stats
-data_dbscan = load_data(1,10,strcat('stats_dbscan_fusion',experiment));
+if (strcmp(experiment, '_11_') == 1 || strcmp(experiment,'_12_') == 1) 
+  printf('Here')
+  data_dbscan = load_data(1,n_max,strcat('stats_dbscan_fusion',experiment));
+else
+  data_dbscan = load_data(1,n_max,strcat('stats_dbscan',experiment));
+endif
 a = get_stats(data_dbscan, dt);
 plot(a(:,7), a(:,1), "linewidth", lw);
-data_lidar = load_data(1,10,strcat('stats_lidar',experiment));
+data_lidar = load_data(1,n_max,strcat('stats_lidar',experiment));
 b = get_stats(data_lidar, dt);
 hold on;
 plot(b(:,7), b(:,1),'g', "linewidth", lw);
-data_fusion = load_data(1,10,strcat('stats_fusion',experiment));
-c = get_stats(data_fusion, dt);
-plot(c(:,7), c(:,1),'r', "linewidth", lw);
-data_radar = load_data(1,10,strcat('stats_radar',experiment));
+if (strcmp(experiment, '_11_') == 1 || strcmp(experiment,'_12_') == 1)
+  data_fusion = load_data(1,n_max,strcat('stats_fusion',experiment));
+  c = get_stats(data_fusion, dt);
+  plot(c(:,7), c(:,1),'r', "linewidth", lw);
+endif
+data_radar = load_data(1,n_max,strcat('stats_radar',experiment));
 d = get_stats(data_radar, dt);
 plot(d(:,7), d(:,1),'k', "linewidth", lw);
 axis("labely")
@@ -43,7 +53,9 @@ hax2 = subplot ("position", [left pos width v_size]);
 plot(a(:,7), a(:,2),'b', "linewidth", lw);
 hold on;
 plot(b(:,7), b(:,2),'g', "linewidth", lw);
-plot(c(:,7), c(:,2),'r', "linewidth", lw);
+if (strcmp(experiment, '_11_') == 1 || strcmp(experiment,'_12_') == 1)
+  plot(c(:,7), c(:,2),'r', "linewidth", lw);
+endif
 plot(d(:,7), d(:,2),'k', "linewidth", lw);
 axis("labely")
 hy2 = ylabel("Orientation error (rad)")
@@ -55,7 +67,9 @@ hax3 = subplot ("position", [left pos width v_size]);
 plot(a(:,7), a(:,3),'b', "linewidth", lw);
 hold on;
 plot(b(:,7), b(:,3),'g', "linewidth", lw);
-plot(c(:,7), c(:,3),'r', "linewidth", lw);
+if (strcmp(experiment, '_11_') == 1 || strcmp(experiment,'_12_') == 1)
+  plot(c(:,7), c(:,3),'r', "linewidth", lw);
+endif
 plot(d(:,7), d(:,3),'k', "linewidth", lw);
 axis("labely")
 hy3 = ylabel("AMCL uncert. (m)")
@@ -65,7 +79,9 @@ hax4 = subplot ("position", [left margin width v_size]);
 plot(a(:,7), a(:,4),'b', "linewidth", lw);
 hold on;
 plot(b(:,7), b(:,4),'g', "linewidth", lw);
-plot(c(:,7), c(:,4),'r', "linewidth", lw);
+if (strcmp(experiment, '_11_') == 1 || strcmp(experiment,'_12_') == 1)
+  plot(c(:,7), c(:,4),'r', "linewidth", lw);
+endif
 plot(d(:,7), d(:,4),'k', "linewidth", lw);
 hx1 = xlabel ("Time (s)");
 set (hx1, "fontsize", fontsize) 
