4861425acb9d34988c41d91944e652b3379d4a22
david-alejo <churr.te@gmail.com>
2021-09-20 13:47:00 +0200

    Final octave scripts

--- octave/plot_radar_stats.m
+++ octave/plot_radar_stats.m
@@ -4,10 +4,12 @@ function plot_radar_stats(d)
   fontsize = 28;
   lw = 3;
   d(:,1)=d(:,1) - d(1,1);
-  hx = plot(d(:,1), d(:,2),'b', "linewidth", lw);
+  hx = plot(d(:,1), d(:,2),'g', "linewidth", lw); % LiDAR --> green
   hold on;
-  plot(d(:,1), d(:,3),'r', "linewidth", lw);
-  plot(d(:,1), d(:,4),'k', "linewidth", lw);
+  plot(d(:,1), d(:,3),'r', "linewidth", lw); % RADAR --> red
+  plot(d(:,1), d(:,4),'b', "linewidth", lw); % Fused --> blue
+  plot(d(:,1), d(:,5),'k', "linewidth", lw); % Cluster --> black
+  plot(d(:,1), d(:,6),'m', "linewidth", lw); % Fritsche --> magenta
   x1 = xlabel ("Time (s)");
   y1 = ylabel("Number of points per measure")
   set (y1, "fontsize", fontsize) 
