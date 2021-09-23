debcc0680604fde0de141172eb363e26c3b500bb
david-alejo <churr.te@gmail.com>
2021-09-23 16:14:31 +0200

    Call to experiment batch simplified

--- scripts/experiment_batch.bash
+++ scripts/experiment_batch.bash
@@ -1,16 +1,24 @@
 #! /bin/bash
+if [[ $# -ne 2 ]]
+then
+  echo "Usage: $0 <min_iteration> <max_iteration>"
+fi
+
 CONTADOR=$1
-export X=6.25
-export Y=4.5
-export A=1.55
-export BAG=2020-12-01-12-04-38_gt_fusion.bag
-until [ $CONTADOR -gt $2 ]; do
+
+until [ "$CONTADOR" -gt "$2" ]; do
+
   # Roslaunch with multiple parameters
-  echo Launch experiment $CONTADOR
-  roslaunch radar_experiments localize_with_gt.launch gt_filename:=stats_segmented_12_$CONTADOR.txt localize_method:=fusion initial_pose_x:=$X initial_pose_y:=$Y initial_pose_a:=$A bag_name:=$BAG scan_topic:=/dbscan_fuser/segmented_scan
-  roslaunch radar_experiments localize_with_gt.launch gt_filename:=stats_dbscan_lines_12_$CONTADOR.txt localize_method:=fusion initial_pose_x:=$X initial_pose_y:=$Y initial_pose_a:=$A bag_name:=$BAG
-  roslaunch radar_experiments localize_with_gt.launch gt_filename:=stats_radar_12_$CONTADOR.txt localize_method:=radar scan_topic:=radar_scan initial_pose_x:=$X initial_pose_y:=$Y initial_pose_a:=$A bag_name:=$BAG
-  roslaunch radar_experiments localize_with_gt.launch gt_filename:=stats_lidar_12_$CONTADOR.txt localize_method:=lidar scan_topic:=lidar_scan initial_pose_x:=$X initial_pose_y:=$Y initial_pose_a:=$A bag_name:=$BAG
-  roslaunch radar_experiments localize_with_gt_fritsche.launch gt_filename:=stats_fritsche2_12_$CONTADOR.txt bag_name:=$BAG initial_pose_x:=$X initial_pose_y:=$Y initial_pose_a:=$A  range_filename:=radar_stats_fritsche_12.m
-  let CONTADOR+=1
+
+  echo Launch repetition $CONTADOR
+  for ((i=1;i<=7;i++))
+  do
+    echo Launch experiment $i
+    roslaunch radar_experiments localize_with_gt.launch gt_filename:=stats_clustered_experiment_${i}_$CONTADOR.txt localize_method:=fusion  bag_name:="published/experiment_${i}.bag" scan_topic:=/dbscan_fuser/segmented_scan points_stats_filename:="points_experiment_${i}.m"
+    roslaunch radar_experiments localize_with_gt.launch gt_filename:=stats_dbscan_lines_experiment_${i}_$CONTADOR.txt localize_method:=fusion bag_name:="experiment_${i}.bag" points_stats_filename:="points_experiment_${i}.m"
+    roslaunch radar_experiments localize_with_gt.launch gt_filename:=stats_radar_experiment_${i}_$CONTADOR.txt localize_method:=radar scan_topic:=radar_scan bag_name:="experiment_${i}.bag" points_stats_filename:="points_experiment_${i}.m"
+    roslaunch radar_experiments localize_with_gt.launch gt_filename:=stats_lidar_experiment_${i}_$CONTADOR.txt localize_method:=lidar scan_topic:=lidar_scan bag_name:="experiment_${i}.bag" points_stats_filename:="points_experiment_${i}.m"
+    roslaunch radar_experiments localize_with_gt_fritsche.launch gt_filename:=stats_fritsche2_experiment_${i}_$CONTADOR.txt bag_name:="experiment_${i}.bag" range_filename:="radar_stats_fritsche_experiment_${i}.m"
+  done
+  (( CONTADOR+=1 )) 
 done
---
+++ stats/README.txt
@@ -0,0 +1,5 @@
+This folder will hold the statistics that were presented in the paper. 
+
+Files stats_* have the data presented in Table 2
+Files points_* have the data presented in Table_1, with the exception of the Fritsche method, whose data is in "radar_stats_fritsche_experiment_..."
+
