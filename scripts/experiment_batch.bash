cbabd64241091db35d60afa84ae9b3b96087541c
David Alejo Teissière <churr.te@gmail.com>
2021-10-06 10:32:27 +0200

    Update experiment_batch.bash

--- scripts/experiment_batch.bash
+++ scripts/experiment_batch.bash
@@ -14,7 +14,7 @@ until [ "$CONTADOR" -gt "$2" ]; do
   for ((i=1;i<=7;i++))
   do
     echo Launch experiment $i
-    roslaunch radar_experiments localize_with_gt.launch gt_filename:=stats_clustered_experiment_${i}_$CONTADOR.txt localize_method:=fusion  bag_name:="published/experiment_${i}.bag" scan_topic:=/dbscan_fuser/segmented_scan points_stats_filename:="points_experiment_${i}.m"
+    roslaunch radar_experiments localize_with_gt.launch gt_filename:=stats_clustered_experiment_${i}_$CONTADOR.txt localize_method:=fusion  bag_name:="experiment_${i}.bag" scan_topic:=/dbscan_fuser/segmented_scan points_stats_filename:="points_experiment_${i}.m"
     roslaunch radar_experiments localize_with_gt.launch gt_filename:=stats_dbscan_lines_experiment_${i}_$CONTADOR.txt localize_method:=fusion bag_name:="experiment_${i}.bag" points_stats_filename:="points_experiment_${i}.m"
     roslaunch radar_experiments localize_with_gt.launch gt_filename:=stats_radar_experiment_${i}_$CONTADOR.txt localize_method:=radar scan_topic:=radar_scan bag_name:="experiment_${i}.bag" points_stats_filename:="points_experiment_${i}.m"
     roslaunch radar_experiments localize_with_gt.launch gt_filename:=stats_lidar_experiment_${i}_$CONTADOR.txt localize_method:=lidar scan_topic:=lidar_scan bag_name:="experiment_${i}.bag" points_stats_filename:="points_experiment_${i}.m"
